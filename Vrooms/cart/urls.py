f922ca50b4bc6a611d0d407c08dcefbe8a26389b
Алексеев Александр <alexxseev@yandex.ru>
2023-03-24 10:12:10 +0500

    add

---
+++ Vrooms/cart/urls.py
@@ -0,0 +1,11 @@
+from django.urls.conf import path
+from django.urls import re_path as url
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.cart_add, name='cart_add'),
+    path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
+]
