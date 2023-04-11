2a0d372cd79b2f0ed436a3f6266c57bbfcdce48d
Алексеев Александр <alexxseev@yandex.ru>
2023-04-11 16:28:08 +0500

    add

--- Vrooms/orders/models.py
+++ Vrooms/orders/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from shop.models import Product
-
+from django.core.validators import RegexValidator
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(blank=True, max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(blank=True)
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(blank=True, max_length=20)
-    city = models.CharField(blank=True, max_length=100)
+    email = models.EmailField(blank=False)
+    room = models.CharField(blank=True, max_length=3)
+    phoneNumberRegex = RegexValidator(regex = r"^\+?1?\d{8,15}$")
+    phoneNumber = models.CharField(validators = [phoneNumberRegex], max_length = 16, unique = False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
