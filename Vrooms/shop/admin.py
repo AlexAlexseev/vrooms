98af5492e5f3c5b2629a7c0ea2b66698a85abb95
Алексеев Александр <alexxseev@yandex.ru>
2023-04-29 23:56:07 +0500

    $

--- Vrooms/shop/admin.py
+++ Vrooms/shop/admin.py
@@ -30,7 +30,7 @@ class ProductAdmin(admin.ModelAdmin):
     # list_display = ['name', 'slug', 'price', 'description', 'weight', 'available', 'created', 'updated']
     # list_filter = ['available', 'created', 'updated']
     # list_editable = ['price', 'available', 'description', 'weight']
-    # prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
     inlines = [DopInline]
     # inlines = [DopInLine]
 
